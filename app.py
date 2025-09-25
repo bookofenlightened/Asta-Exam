4fc67b84013f79f43411a2ecdab2daa505cf172b
bookofenlightened <aaayo9338@gmail.com>
2025-09-25 15:24:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -76,3 +76,7 @@ def upload():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render থেকে PORT নেবে
+    app.run(host="0.0.0.0", port=port)
